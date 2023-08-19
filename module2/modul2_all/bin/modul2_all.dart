3d47a7f134b74dfe2fd7f33cf6c80769a230d265
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-08-19 12:05:14 +0600

    initial commit

---
+++ module2/modul2_all/bin/modul2_all.dart
@@ -0,0 +1,5 @@
+import 'package:modul2_all/modul2_all.dart' as modul2_all;
+
+void main(List<String> arguments) {
+  print('Hello world: ${modul2_all.calculate()}!');
+}
---
+++ module2/modul2_all/test/modul2_all_test.dart
@@ -0,0 +1,8 @@
+import 'package:modul2_all/modul2_all.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
